d8d526e4d425c89ff11c6f8afae434f9d8239d9d
AnnyshkaF <AnnyshkaF@github.com>
2021-10-21 22:01:52 +0300

    Linting (run.py)

--- Run.py
+++ Run.py
@@ -6,61 +6,58 @@ Uses YOLO to obtain 2D box, PyTorch to get 3D box, plots both
 SPACE bar for next image, any other key to exit
 """
 
-
-from torch_lib.Dataset import *
-from library.Math import *
-from library.Plotting import *
-from torch_lib import Model, ClassAverages
-from yolo.yolo import cv_Yolo
-
 import os
+import sys
 import time
-
+import argparse
 import numpy as np
 import cv2
 
+
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
 from torchvision.models import vgg
+from torch_lib.Dataset import DetectedObject, generate_bins
+from torch_lib import Model, ClassAverages
+from library.Math import calc_location
+from library.Plotting import plot_2d_box, plot_3d_box
+from yolo.yolo import cv_Yolo
 
-import argparse
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+def str2bool(v_str):
+    """ convertes string to bool """
+    if v_str.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v_str.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
-parser = argparse.ArgumentParser()
+PARSER = argparse.ArgumentParser()
 
-parser.add_argument("--image-dir", default="eval/image_2/",
+PARSER.add_argument("--image-dir", default="eval/image_2/",
                     help="Relative path to the directory containing images to detect. Default \
                     is eval/image_2/")
 
-# TODO: support multiple cal matrix input types
-parser.add_argument("--cal-dir", default="camera_cal/",
+PARSER.add_argument("--cal-dir", default="camera_cal/",
                     help="Relative path to the directory containing camera calibration form KITTI. \
                     Default is camera_cal/")
 
-parser.add_argument("--video", action="store_true",
+PARSER.add_argument("--video", action="store_true",
                     help="Weather or not to advance frame-by-frame as fast as possible. \
                     By default, this will pull images from ./eval/video")
 
-parser.add_argument("--show-yolo", action="store_true",
+PARSER.add_argument("--show-yolo", action="store_true",
                     help="Show the 2D BoundingBox detecions on a separate image")
 
-parser.add_argument("--hide-debug", action="store_true",
+PARSER.add_argument("--hide-debug", action="store_true",
                     help="Supress the printing of each 3d location")
 
 
 def plot_regressed_3d_bbox(img, cam_to_img, box_2d, dimensions, alpha, theta_ray, img_2d=None):
-
+    """ plots regressed 3d box """
     # the math! returns X, the corners used for constraint
-    location, X = calc_location(dimensions, cam_to_img, box_2d, alpha, theta_ray)
+    location, [] = calc_location(dimensions, cam_to_img, box_2d, alpha, theta_ray)
 
     orient = alpha + theta_ray
 
@@ -72,19 +69,18 @@ def plot_regressed_3d_bbox(img, cam_to_img, box_2d, dimensions, alpha, theta_ray
     return location
 
 def main():
-
-    FLAGS = parser.parse_args()
+    """ main function """
+    flags = PARSER.parse_args()
 
     # load torch
     weights_path = os.path.abspath(os.path.dirname(__file__)) + '/weights'
     model_lst = [x for x in sorted(os.listdir(weights_path)) if x.endswith('.pkl')]
     if len(model_lst) == 0:
         print('No previous model found, please train first!')
-        exit()
+        sys.exit()
     else:
         print('Using previous model %s'%model_lst[-1])
         my_vgg = vgg.vgg19_bn(pretrained=True)
-        # TODO: load bins from file or something
         model = Model.Model(features=my_vgg.features, bins=2).cuda()
         checkpoint = torch.load(weights_path + '/%s'%model_lst[-1])
         model.load_state_dict(checkpoint['model_state_dict'])
@@ -96,13 +92,12 @@ def main():
 
     averages = ClassAverages.ClassAverages()
 
-    # TODO: clean up how this is done. flag?
     angle_bins = generate_bins(2)
 
-    image_dir = FLAGS.image_dir
-    cal_dir = FLAGS.cal_dir
-    if FLAGS.video:
-        if FLAGS.image_dir == "eval/image_2/" and FLAGS.cal_dir == "camera_cal/":
+    image_dir = flags.image_dir
+    cal_dir = flags.cal_dir
+    if flags.video:
+        if flags.image_dir == "eval/image_2/" and flags.cal_dir == "camera_cal/":
             image_dir = "eval/video/2011_09_26/image_2/"
             cal_dir = "eval/video/2011_09_26/"
 
@@ -117,9 +112,9 @@ def main():
 
     try:
         ids = [x.split('.')[0] for x in sorted(os.listdir(img_path))]
-    except:
+    except ValueError:
         print("\nError: no images in %s"%img_path)
-        exit()
+        sys.exit()
 
     for img_id in ids:
 
@@ -141,21 +136,20 @@ def main():
             if not averages.recognized_class(detection.detected_class):
                 continue
 
-            # this is throwing when the 2d bbox is invalid
-            # TODO: better check
             try:
-                detectedObject = DetectedObject(img, detection.detected_class, detection.box_2d, calib_file)
-            except:
+                det_obj = DetectedObject( \
+                    img, detection.detected_class, detection.box_2d, calib_file)
+            except ValueError:
                 continue
 
-            theta_ray = detectedObject.theta_ray
-            input_img = detectedObject.img
-            proj_matrix = detectedObject.proj_matrix
+            theta_ray = det_obj.theta_ray
+            input_img = det_obj.img
+            proj_matrix = det_obj.proj_matrix
             box_2d = detection.box_2d
             detected_class = detection.detected_class
 
-            input_tensor = torch.zeros([1,3,224,224]).cuda()
-            input_tensor[0,:,:,:] = input_img
+            input_tensor = torch.zeros([1, 3, 224, 224]).cuda()
+            input_tensor[0, :, :, :] = input_img
 
             [orient, conf, dim] = model(input_tensor)
             orient = orient.cpu().data.numpy()[0, :, :]
@@ -172,30 +166,32 @@ def main():
             alpha += angle_bins[argmax]
             alpha -= np.pi
 
-            if FLAGS.show_yolo:
-                location = plot_regressed_3d_bbox(img, proj_matrix, box_2d, dim, alpha, theta_ray, truth_img)
+            if flags.show_yolo:
+                location = plot_regressed_3d_bbox( \
+                    img, proj_matrix, box_2d, dim, alpha, theta_ray, truth_img)
             else:
-                location = plot_regressed_3d_bbox(img, proj_matrix, box_2d, dim, alpha, theta_ray)
+                location = plot_regressed_3d_bbox( \
+                    img, proj_matrix, box_2d, dim, alpha, theta_ray)
 
-            if not FLAGS.hide_debug:
+            if not flags.hide_debug:
                 print('Estimated pose: %s'%location)
 
-        if FLAGS.show_yolo:
+        if flags.show_yolo:
             numpy_vertical = np.concatenate((truth_img, img), axis=0)
             cv2.imshow('SPACE for next image, any other key to exit', numpy_vertical)
         else:
             cv2.imshow('3D detections', img)
 
-        if not FLAGS.hide_debug:
+        if not flags.hide_debug:
             print("\n")
             print('Got %s poses in %.3f seconds'%(len(detections), time.time() - start_time))
             print('-------------')
 
-        if FLAGS.video:
+        if flags.video:
             cv2.waitKey(1)
         else:
             if cv2.waitKey(0) != 32: # space bar
-                exit()
+                sys.exit()
 
 if __name__ == '__main__':
     main()
