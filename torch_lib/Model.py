93b908f0be5421247538be2055abc8de9cee9025
AnnyshkaF <AnnyshkaF@github.com>
2021-10-22 00:03:35 +0300

    Linting (Dataset.py, Model.py)

--- torch_lib/Model.py
+++ torch_lib/Model.py
@@ -1,66 +1,68 @@
+"""
+Model class
+"""
+
 import torch
-import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
-
-def OrientationLoss(orient_batch, orientGT_batch, confGT_batch):
-
+def OrientationLoss(orient_batch, or_gt_batch, conf_gt_batch):
+    """ Orientation Loss """
     batch_size = orient_batch.size()[0]
-    indexes = torch.max(confGT_batch, dim=1)[1]
+    indexes = torch.max(conf_gt_batch, dim=1)[1]
 
     # extract just the important bin
-    orientGT_batch = orientGT_batch[torch.arange(batch_size), indexes]
+    or_gt_batch = or_gt_batch[torch.arange(batch_size), indexes]
     orient_batch = orient_batch[torch.arange(batch_size), indexes]
 
-    theta_diff = torch.atan2(orientGT_batch[:,1], orientGT_batch[:,0])
-    estimated_theta_diff = torch.atan2(orient_batch[:,1], orient_batch[:,0])
+    theta_diff = torch.atan2(or_gt_batch[:, 1], or_gt_batch[:, 0])
+    estimated_theta_diff = torch.atan2(orient_batch[:, 1], orient_batch[:, 0])
 
     return -1 * torch.cos(theta_diff - estimated_theta_diff).mean()
 
 class Model(nn.Module):
-    def __init__(self, features=None, bins=2, w = 0.4):
+    """ Class Model """
+    def __init__(self, features=None, bins=2, weigth=0.4):
+        """ Initialize class """
         super(Model, self).__init__()
         self.bins = bins
-        self.w = w
+        self.w = weigth
         self.features = features
-        self.orientation = nn.Sequential(
-                    nn.Linear(512 * 7 * 7, 256),
-                    nn.ReLU(True),
-                    nn.Dropout(),
-                    nn.Linear(256, 256),
-                    nn.ReLU(True),
-                    nn.Dropout(),
-                    nn.Linear(256, bins*2) # to get sin and cos
-                )
-        self.confidence = nn.Sequential(
-                    nn.Linear(512 * 7 * 7, 256),
-                    nn.ReLU(True),
-                    nn.Dropout(),
-                    nn.Linear(256, 256),
-                    nn.ReLU(True),
-                    nn.Dropout(),
-                    nn.Linear(256, bins),
-                    # nn.Softmax()
-                    #nn.Sigmoid()
-                )
-        self.dimension = nn.Sequential(
-                    nn.Linear(512 * 7 * 7, 512),
-                    nn.ReLU(True),
-                    nn.Dropout(),
-                    nn.Linear(512, 512),
-                    nn.ReLU(True),
-                    nn.Dropout(),
-                    nn.Linear(512, 3)
-                )
+        self.orientation = nn.Sequential(\
+            nn.Linear(512 * 7 * 7, 256),\
+            nn.ReLU(True),\
+            nn.Dropout(),\
+            nn.Linear(256, 256),\
+            nn.ReLU(True),\
+            nn.Dropout(),\
+            nn.Linear(256, bins*2)\
+        )
+        self.confidence = nn.Sequential(\
+            nn.Linear(512 * 7 * 7, 256),\
+            nn.ReLU(True),\
+            nn.Dropout(),\
+            nn.Linear(256, 256),\
+            nn.ReLU(True),\
+            nn.Dropout(),\
+            nn.Linear(256, bins),\
+        )
+        self.dimension = nn.Sequential(\
+            nn.Linear(512 * 7 * 7, 512),\
+            nn.ReLU(True),\
+            nn.Dropout(),\
+            nn.Linear(512, 512),\
+            nn.ReLU(True),\
+            nn.Dropout(),\
+            nn.Linear(512, 3)\
+        )
 
-    def forward(self, x):
-        x = self.features(x) # 512 x 7 x 7
-        x = x.view(-1, 512 * 7 * 7)
-        orientation = self.orientation(x)
+    def forward(self, val_x):
+        """ Forward propagation """
+        val_x = self.features(val_x) # 512 x 7 x 7
+        val_x = val_x.view(-1, 512 * 7 * 7)
+        orientation = self.orientation(val_x)
         orientation = orientation.view(-1, self.bins, 2)
         orientation = F.normalize(orientation, dim=2)
-        confidence = self.confidence(x)
-        dimension = self.dimension(x)
+        confidence = self.confidence(val_x)
+        dimension = self.dimension(val_x)
         return orientation, confidence, dimension
