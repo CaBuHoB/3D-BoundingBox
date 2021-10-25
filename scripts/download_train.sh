33ec1700ddac175754a07c010d68f3db469ca05d
Maxim Savinov <cabuhob@gmail.com>
2021-10-25 10:02:10 +0300

    Add scripts

---
+++ scripts/download_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+echo "Downloading dataset ..."
+wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_image_2.zip -O Kitti/DOI.zip
+wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_calib.zip -O Kitti/DOC.zip
+wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_label_2.zip -O Kitti/DOL.zip
+
+echo "Uncompressing dataset ..."
+unzip -u Kitti/DOI.zip -d Kitti/
+unzip -u Kitti/DOC.zip -d Kitti/
+unzip -u Kitti/DOI.zip -d Kitti/
+
+echo "Remove dataset zips ..."
+rm Kitti/DOI.zip 
+rm Kitti/DOC.zip 
+rm Kitti/DOL.zip 
---
+++ scripts/eval.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+
+python Run.py --video --hide-debug
